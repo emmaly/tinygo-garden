2a0029eb928189143bf240d9a75e93758097be8d
Emmaly <emmaly@emma.ly>
2023-01-21 19:55:06 -0800

    Added plasmastick2040w/ws2812-strip

---
+++ plasmastick2040w/ws2812-strip/go.mod
@@ -0,0 +1,5 @@
+module github.com/emmaly/tinygo-garden/plasmastick2040w/ws2812-strip
+
+go 1.19
+
+require tinygo.org/x/drivers v0.23.0
---
+++ plasmastick2040w/ws2812-strip/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"image/color"
+	"machine"
+	"math/rand"
+	"time"
+
+	"tinygo.org/x/drivers/ws2812"
+)
+
+var stripLeds [64]color.RGBA
+
+func main() {
+	stripPin := machine.GPIO15
+	stripPin.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	strip := ws2812.New(stripPin)
+
+	for {
+		for i := 0; i < len(stripLeds); i++ {
+			red := uint8(rand.Uint32() & uint32(0x16))
+			green := uint8(rand.Uint32() & uint32(0x16))
+			blue := uint8(rand.Uint32() & uint32(0x16))
+			stripLeds[i] = color.RGBA{R: red, G: green, B: blue}
+		}
+		strip.WriteColors(stripLeds[:])
+		time.Sleep(time.Duration(rand.Uint32() & 500) * time.Millisecond)
+	}
+}
